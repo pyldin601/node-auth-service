e57d812e54d9ee4c406d05ae62b4811f35668ccc
Roman <roman.lakhtadyr@gmail.com>
2021-02-19 01:57:24 +0200

    feat: add migrations to the auth service

---
+++ migrations/20200208224930_createRefreshTokensTable.js
@@ -0,0 +1,17 @@
+exports.up = function up(knex) {
+  return knex.schema.createTable("refresh_tokens", table => {
+    table.increments();
+    table
+      .integer("user_id")
+      .references("users.id")
+      .notNullable();
+    table.string("refresh_token").notNullable();
+    table.timestamps();
+
+    table.unique(["refresh_token"]);
+  });
+};
+
+exports.down = function down(knex) {
+  return knex.schema.dropTable("refresh_tokens");
+};
