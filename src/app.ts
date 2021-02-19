c433395fa8dce90268101b9f0f81d9b11eb355c6
Roman <roman.lakhtadyr@gmail.com>
2021-02-19 09:14:32 +0200

    feat: remove old redundant dependencies from auth service

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import cors = require("@koa/cors")
-import { requestLogger } from "@myownradio/shared-server/lib/koa/mw"
 import Application = require("koa")
 import { Config } from "./config"
 import { Knex } from "./knex"
 import { Logger } from "./logger"
 import { createRouter } from "./router"
+import {requestLogger} from "./requestLogger";
+
 
 export function createApp(config: Config, knex: Knex, logger: Logger): Application {
   const app = new Application()
---
+++ src/requestLogger.ts
@@ -0,0 +1,28 @@
+import { Context, Middleware } from "koa"
+
+interface Logger {
+  warn(formatter: string, data: unknown): void
+}
+
+/**
+ * @see https://github.com/koajs/koa/blob/d48d88ee17b780c02123e6d657274cab456e943e/lib/context.js#L150
+ */
+const DEFAULT_KOA_ERROR_STATUS_CODE = 500
+
+export function requestLogger(logger: Logger): Middleware {
+  return async (ctx: Context, next: () => Promise<void>): Promise<void> => {
+    try {
+      await next()
+    } catch (error) {
+      const route = ctx._matchedRoute
+      const { method } = ctx.request
+
+      const statusCode = error.status || DEFAULT_KOA_ERROR_STATUS_CODE
+      const errorText = (error.stack || error) as string
+
+      logger.warn(`Error happened during inbound http request: ${errorText}`, { method, route, statusCode })
+
+      throw error
+    }
+  }
+}
